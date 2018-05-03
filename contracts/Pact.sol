ed395b970b4789edf2989ac55df3165e9333393d
Igor Ermakov <37930501+Millfort@users.noreply.github.com>
2018-05-03 06:37:23 +0300

    Update Pact.sol

--- contracts/Pact.sol
+++ contracts/Pact.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.18;
 
 contract Pact {
     
@@ -15,11 +15,34 @@ contract Pact {
         uint256 time;
         bool    ended;
     }
+
+     event NewPact(
+        uint256 beneficiary,
+        uint256 principal,
+        uint256 money,
+        uint256 date,
+        uint256 time
+    );
+    
+    event CompletePact(
+        uint256 beneficiary,
+        uint256 principal,
+        uint256 money
+    );
+    
+    event NoSuchPact(
+        uint id
+    );
     
     function completeContract(uint id) public {
         if(msg.sender != bank) {
             return;
         }
+        if(id > pact_id) {
+            NoSuchPact(id);
+            return;
+        }
+            
         pacts[id].ended = true;
     }
     
@@ -33,19 +56,18 @@ contract Pact {
         pacts[pact_id].date = now;
         pacts[pact_id].time = now + _time * 1 days;
         pact_id++;
+        NewPact(_benefeciary, _principal, _money, now, _time);
     }
     
-    function getData(uint id) public constant returns(uint256[]) {
+    function getData(uint id) public constant returns(uint256, uint256, uint256,uint256, uint256) {
         if(msg.sender != bank) {
             return;
         }
-        uint256[] memory data = new uint256[](5);
-        data[0] = pacts[id].beneficiary;
-        data[1] = pacts[id].principal;
-        data[2] = pacts[id].money;
-        data[3] = pacts[id].date;
-        data[4] = pacts[id].time;
-        return data;
+        if(id > pact_id) {
+            NoSuchPact(id);
+            return;
+        }
+        return (pacts[id].beneficiary, pacts[id].principal, pacts[id].money, pacts[id].date, pacts[id].time);
     }
     
     function Pact() public {
