69972241e8aaf36be29cc946883c9ee77aee22ad
Eugene <AcCakyra@inbox.ru>
2018-07-03 19:01:58 +0700

    Update BankGuarantee.sol

--- BankGuarantee.sol
+++ BankGuarantee.sol
@@ -6,8 +6,9 @@ contract BankGuarantee is usingOraclize {
     // should be guarantor's address
     // only owner has a access
     address public owner = msg.sender;
-    
-    uint pact_id = 0;
+    // global variable for the identification of pacts
+    uint private pact_id = 0;
+    uint maxValue = 20000; // guarantees limit. in your prefer currency
 
     // security check
     // only guarantor can get access
@@ -20,6 +21,8 @@ contract BankGuarantee is usingOraclize {
         _;
     }
     
+    // whitelist check
+    // only whitelisted user can get access
     modifier onlyWhitelistUser() {
         require (
             // TODO: check with BankData
@@ -35,42 +38,85 @@ contract BankGuarantee is usingOraclize {
         owner = newOwner;
     }
     
-    mapping (uint => Pacts) pacts;
-    HeapNode[] public heap;
-
+    // the main data structures for contract
+    // think about change to T[]
+    mapping (uint => Pacts) private pacts;
+    HeapNode[] private heap;
+    bool private lock = false;
+    
     struct HeapNode {
-        uint256 time; // key
+        uint end_time; // key
         uint id; // value
     }
+    
+    struct Pacts {
+        uint    id;
+        address principal;
+        address beneficiary; 
+        uint    money;
+        uint    start_time; // when pacts is start
+        uint    end_time;   // when pacts is end
+        bool    accept;
+        bool    ended;
+    }
+    
+    function createPactByPrincipal(address _beneficiary, uint _money, uint _end_time) public onlyWhitelistUser() returns (uint) {
+        require(_money <= maxValue, "Cash limit exceeded.");
+        uint id = pact_id++;
+        pacts[id].principal = msg.sender;
+        pacts[id].beneficiary = _beneficiary;
+        pacts[id].money = _money;
+        pacts[id].end_time = _end_time;
+        return id;
+    }
+    
+    function acceptContractByBeneficiary(address _principal, uint _money, uint _end_time, uint _pact_id) public onlyWhitelistUser() returns(bool) {
+        if (lock == true) return false;
+        require(_end_time > now, "end_time cannot be less than now. It is impossible.");
+        lock = true;
+        uint id = pact_id;
+        if ((pacts[id].principal == _principal) && (pacts[id].beneficiary == msg.sender) && (pacts[id].money == _money) && (pacts[id].end_time == _end_time)) {
+            pacts[id].accept = true;   
+            pacts[id].start_time = now;
+            add(_end_time - pacts[id].start_time, id);
+        }
+        lock = false;
+        return true;
+    }
+    
+    function getData(uint id) public view returns(address, address, uint, uint, uint, bool, bool) {
+        return (pacts[id].beneficiary, pacts[id].principal, pacts[id].money, pacts[id].start_time, pacts[id].end_time, pacts[id].accept, pacts[id].ended);
+    }
+    
+    function completeContract(uint id) public {
+        pacts[id].ended = true;
+    }
 
-    function add(uint256 time, uint id) public {
-        HeapNode memory node = HeapNode(time, id);
-        heap.push(node);
+    // simple classical heap implementation
+    // HEAP-->>
+    function add(uint end_time, uint id) private {
+        heap.push(HeapNode(end_time, id));
         
         uint i = heap.length - 1;
         uint parent = (i - 1) / 2;
         
-        while (i > 0 && heap[parent].time < heap[i].time) {
+        while (i > 0 && heap[parent].end_time < heap[i].end_time) {
             HeapNode memory temp = heap[i];
-            heap[i].time = heap[parent].time;
-            heap[i].id = heap[parent].id;
-            heap[parent].time = temp.time;
-            heap[parent].id = temp.id;
-
+            heap[i] = heap[parent];
+            heap[parent] = temp;
             i = parent;
             parent = (i - 1) / 2;
         }
     }
     
-  
-
-    function getMax() public {
-        
-        heap[0].time = heap[heap.length - 1].time;
-        heap[0].id = heap[heap.length - 1].id;
+    function getMax() private returns (uint end_time, uint id) {
+        HeapNode memory result = heap[0];
+        heap[0] = heap[heap.length - 1];
         delete heap[heap.length - 1];
         heap.length--;
+        
         heapify(0);
+        return (result.end_time, result.id);
     }
     
     function heapify(uint i) private {
@@ -83,94 +129,61 @@ contract BankGuarantee is usingOraclize {
             rightChild = 2 * i + 2;
             largestChild = i;
 
-            if (leftChild < heap.length && heap[leftChild].time > heap[largestChild].time) {
+            if (leftChild < heap.length && heap[leftChild].end_time > heap[largestChild].end_time) {
                 largestChild = leftChild;
             }
-            if (rightChild < heap.length && heap[rightChild].time > heap[largestChild].time) {
+            if (rightChild < heap.length && heap[rightChild].end_time > heap[largestChild].end_time) {
                 largestChild = rightChild;
             }
-            if (largestChild == i) {
-                break;
-            }
+            if (largestChild == i) break;
 
-            HeapNode temp;
-            temp.time = heap[i].time;
-            temp.id = heap[i].id;
+            HeapNode memory temp = heap[i];
             
-            heap[i].id = heap[largestChild].id;
-            heap[i].time = heap[largestChild].time;
-            heap[largestChild].time = temp.time;
-            heap[largestChild].id = temp.id;
+            heap[i] = heap[largestChild];
+            heap[largestChild] = temp;
             i = largestChild;
         }
     }
+    //<<--HEAP
     
-    struct Pacts {
-        uint    id;
-        address beneficiary;
-        address principal;
-        uint256 money;
-        uint256 date;
-        uint256 time;
-        bool    accept;
-        bool    ended;
-    }
-    
-    function createPactByPrincipal(address _beneficiary, uint256 _money, uint256 _time) public onlyWhitelistUser() returns (uint) {
-        pacts[pact_id].principal = msg.sender;
-        pacts[pact_id].beneficiary = _beneficiary;
-        pacts[pact_id].money = _money;
-        pacts[pact_id].date = now;
-        pacts[pact_id].time = _time;
-        pact_id++;
-        return pact_id - 1;
-    }
-    
-    function acceptContractByBeneficiary(address _principal, uint256 _money, uint256 _time, uint _pact_id) public onlyWhitelistUser() {
-        if (( pacts[_pact_id].principal == _principal) && (pacts[_pact_id].beneficiary == msg.sender) && (pacts[_pact_id].money == _money) && (pacts[_pact_id].time == _time)) {
-            pacts[_pact_id].accept = true;     
-            add(_time, _pact_id);
-        }
-    }
-    
-    function getData(uint id) public view onlyBy(owner) returns(address, address, uint256,uint256, uint256, bool) {
-        return (pacts[id].beneficiary, pacts[id].principal, pacts[id].money, pacts[id].date, pacts[id].time, pacts[id].ended);
-    }
-    
-    function completeContract(uint id) public onlyBy(owner) {
-        pacts[id].ended = true;
-    }
-
+    // it requires some eth for work
+    // this is fallback function
     function() public payable {}
 
-    //TIMER-->>
+    //end_timeR-->>
     event LogConstructorInitiated(string nextStep); // for testing -->>
-    event LogCallback(uint time);
+    event LogCallback(uint end_time);
     event LogNewOraclizeQuery(string description); // <<--for testing
     
     function __callback(bytes32 myid, string result) { 
         if (msg.sender != oraclize_cbAddress()) revert();
         //LogCallback(now); // test 
-        if (pacts[pact_id - 1].time - now > 0) {
-            createTimer(); // again if remain time
+        if (pacts[pact_id - 1].end_time - now > 0) {
+            createend_timer(); // again if remain end_time
         } else {
             completeContract(pact_id - 1);
         }   
     }
 
-    function createTimer() public {
-        
+    function createend_timer() public returns(bool) {
         if (oraclize_getPrice("URL") > address(this).balance ) {
             emit LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
         } else {
-            if (pacts[pact_id - 1].time - pacts[pact_id - 1].date > 60 days) { // max period
+            if (!lock) return false;
+            lock = true;
+            uint end_time;
+            uint id;
+            (end_time, id) = getMax();
+            lock = false;
+            
+            if (end_time > 60 days) { // max period
                 emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-                oraclize_query(60 days,"URL", ""); // check at max time
+                oraclize_query(60 days,"URL", ""); // check at max end_time
             } else {
                 emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-                oraclize_query(pacts[pact_id].time - pacts[pact_id].date,"URL", ""); // check at time
+                oraclize_query(end_time, "URL", ""); // check at end_time
             }
         }
     }
-    //<<--TIMER
+    //<<--end_timeR
 }
